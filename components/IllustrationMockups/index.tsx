b7e44cf4680065d3b580aa3e4d9fcec12497ed19
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-01-03 16:40:10 -0300

    feat: add IllustrationMockups component

---
+++ components/IllustrationMockups/index.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+const IllustrationMockups = () => {
+  return (
+    <Image
+      src={"/assets/illustration-mockups.svg"}
+      alt=""
+      width={300}
+      height={120}
+    />
+  );
+};
+
+export default IllustrationMockups;
