d7e43d16084c5aa4df14f69c9504cc359bc8a23f
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-01-03 16:41:38 -0300

    feat: add components to Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,29 @@
+import Button from "@/components/Button";
+import Icons from "@/components/Icons";
+import IllustrationMockups from "@/components/IllustrationMockups";
+import Logo from "@/components/Logo";
+import Text from "@/components/Text";
+import Title from "@/components/Title";
+
 const Home = () => {
-  return <main>hello</main>;
+  return (
+    <>
+      <header>
+        <Logo />
+      </header>
+      <main>
+        <IllustrationMockups />
+        <Title />
+        <Text />
+        <Button />
+      </main>
+      <footer>
+        <Icons src="/assets/icons/facebook.svg" alt="facebook" />
+        <Icons src="/assets/icons/twitter.svg" alt="twitter" />
+        <Icons src="/assets/icons/instagram.svg" alt="instagram" />
+      </footer>
+    </>
+  );
 };
 
 export default Home;
