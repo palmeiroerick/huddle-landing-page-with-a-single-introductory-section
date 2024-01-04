8421d4141e4808690b2c5e93a66dce2fdb3eaaa5
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-01-04 18:29:55 -0300

    style: add desktop styles to Button component

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -1,5 +1,5 @@
 const Button = () => {
-  return <button className="hover:bg-softMagenta hover:text-white m-auto w-52 h-12 rounded-full bg-white text-violet text-base">Register</button>;
+  return <button className="hover:bg-softMagenta hover:text-white m-auto w-52 h-12 rounded-full bg-white text-violet text-base lg:m-0">Register</button>;
 };
 
 export default Button;
