e2d029d9caf2f214e84dbbac50afb03428e50c9e
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-01-04 15:16:19 -0300

    style: add styles to Button component

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -1,5 +1,5 @@
 const Button = () => {
-  return <button>Register</button>;
+  return <button className="hover:bg-softMagenta hover:text-white m-auto w-52 h-12 rounded-full bg-white text-violet text-base">Register</button>;
 };
 
 export default Button;
