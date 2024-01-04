468d9c369fe4565cd9f474603f44729f78e4bac0
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-01-04 15:16:54 -0300

    style: add styles to Text component

--- components/Text/index.tsx
+++ components/Text/index.tsx
@@ -1,6 +1,6 @@
 const Text = () => {
   return (
-    <p>
+    <p className="text-white text-[0.938rem] font-normal">
       Huddle re-imagines the way we build communities. You have a voice, but so
       does your audience. Create connections with your users as you engage in
       genuine discussion.
