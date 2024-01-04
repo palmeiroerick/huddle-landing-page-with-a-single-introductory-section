571ab3c4ad3e51d87c7e8e4d3cf9137174ae30e2
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-01-04 18:30:25 -0300

    style: add desktop styles to IllustrationMockups component

--- components/IllustrationMockups/index.tsx
+++ components/IllustrationMockups/index.tsx
@@ -2,12 +2,14 @@ import Image from "next/image";
 
 const IllustrationMockups = () => {
   return (
-    <Image
-      src={"/assets/illustration-mockups.svg"}
-      alt=""
-      width={300}
-      height={120}
-    />
+    <div className="relative h-52 w-[19rem] lg:w-[120rem] lg:h-[32rem]">
+      <Image
+        src={"/assets/illustration-mockups.svg"}
+        alt=""
+        objectFit="cover"
+        fill
+      />
+    </div>
   );
 };
 
